fb50c11d08498b74564c7cb379a9212e330f03b4
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-14 12:25:14 +0530

    first commit

--- final_mali.php
+++ final_mali.php
@@ -17,7 +17,34 @@
 		else{
 			echo "Something went wrong!";
 		}
-	   
+		 
+		//Enter your login username 
+$username="xxxxxxx";
+
+//Enter your login password 
+$password="";
+
+//Enter your text message 
+$message="hello";
+
+//Enter your Sender ID
+$sender="";
+
+//Enter your receiver mobile number
+$mobile_number="XXXXXXXXXXX";
+
+//Don't change below code use as it is
+$url="https://www.bulksmsgateway.in/sendmessage.php?user=".urlencode($username)."&password=".urlencode($password)."&
+mobile=".urlencode($mobile_number)."&message=".urlencode($message)."&sender=".urlencode($sender)."&type=".urlencode('3');
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$curl_scraped_page = curl_exec($ch);
+
+curl_close($ch);
+
 	}
 	
 ?>
