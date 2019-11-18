c611837ca7ab19b828ab251d8c2e928c95e12545
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-18 11:26:29 +0530

    second push

--- final_mali.php
+++ final_mali.php
@@ -1,4 +1,28 @@
 <?php
+    $username="Jithendra";
+
+	
+	$password="Jithendra@18";
+	
+
+	$message1="Property_Name :".$subject."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+	
+	
+	$sender="TestID";
+	
+	
+	$mobile_number=9071352854;
+
+	if($_POST['submitted']=='true')
+         { 
+$url="login.bulksmsgateway.in/sendmessage.php?user=".urlencode($username)."&password=".urlencode($password)."&mobile=".urlencode($number)."&sender=".urlencode($sender)."&message=".urlencode($message1)."&type=".urlencode('3'); 
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+echo $curl_scraped_page = curl_exec($ch);
+curl_close($ch); 
+}
+
+
     if(isset($_POST['submit'])){
 		$name=$_POST['client_name'];
 		$email=$_POST['client_mail_id'];
@@ -9,48 +33,15 @@
 		$subject='Assetz';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
-
-
 		if(mail($to, $subject, $message, $headers)){
-			
-		$username="Jithendra";
-
-		//Enter your login password 
-		$password="Jithendra@18";
-		
-		//Enter your text message 
-		$message="Property_Name :".$subject."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		
-		//Enter your Sender ID
-		$sender="TestID";
-		
-		//Enter your receiver mobile number
-		$mobile_number="+919071352854";
-		
-		//Don't change below code use as it is
-		$url="https://www.bulksmsgateway.in/sendmessage.php?user=".urlencode($username)."&password=".urlencode($password)."&
-		mobile=".urlencode($mobile_number)."&message=".urlencode($message)."&sender=".urlencode($sender)."&type=".urlencode('3');
-		
-		$ch = curl_init($url);
-		
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		
-		$curl_scraped_page = curl_exec($ch);
-		
-		curl_close($ch);
-				
+							
 			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 			//Enter your login username 
 
 		}
-		else{
-			echo "Something went wrong!";
-		}
-		 
-
-
+		
 	}
-	
+
 ?>
 
 
