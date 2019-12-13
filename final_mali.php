8973931a17153933d8efc752223855a061cc7592
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-12-13 07:25:11 +0530

    second commit

--- final_mali.php
+++ final_mali.php
@@ -7,42 +7,32 @@
 
 		$to='livexcellence1@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Assetz Marq 2.0';
-		$message="Project_Name:"."Assetz Marq 2.0"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$message="Project_Name:"."Assetz Marq 2.0"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Email :".$email."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
-							
-			echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		$apiKey = urlencode("PC56D9fMjaQ-3gw9h0qV4d5KkwYZMpo5xr1McpRBYv");
+		// Message details
+		$numbers = array(918050933931);
+		$sender = urlencode("TXTLCL");
+		$message = rawurlencode("$message");
 
-			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1-q78u4kZ20jzAK0xLiOsORgOs3-coaiy">Click here to download Brochure</a><h3>';
-			echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1GwUm4pSQygJcefm9JXOcTv46UM6fbkNt">Click here to download Cost Sheet</a><h3>';
-			//Enter your login username 
-			$username="Jithendra";
+		$numbers = implode(",", $numbers);
 
-	
-	        $password="Jithendra@18";
-	
-
-	       $message1="Property_Name :".$subject."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Email :".$email."\n"."Wrote the following :"."\n\n".$msg;
-	
-	
-	           $sender="TestID";
-	
-	
-	          $mobile_number=9071352854;
-
-	if($_POST['submitted']=='true')
-         { 
-		$url="login.bulksmsgateway.in/sendmessage.php?user=".urlencode($username)."&password=".urlencode($password)."&mobile=".urlencode($number)."&sender=".urlencode($sender)."&message=".urlencode($message1)."&type=".urlencode('3'); 
-		$ch = curl_init($url);
+		// Prepare data for POST request
+		$data = array("apikey" => $apiKey, "numbers" => $numbers, "sender" => $sender, "message" => $message);
+		// Send the POST request with cURL
+		$ch = curl_init("https://api.textlocal.in/send/");
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		echo $curl_scraped_page = curl_exec($ch);
-		curl_close($ch); 
-}
-
-
-		}
+		$response = curl_exec($ch);
+		curl_close($ch);
+		echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1-q78u4kZ20jzAK0xLiOsORgOs3-coaiy">Click here to download Brochure</a><h3>';
+		echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1GwUm4pSQygJcefm9JXOcTv46UM6fbkNt">Click here to download Cost Sheet</a><h3>';
 		
-	}
+	    }
+    }
 
 ?>
 
